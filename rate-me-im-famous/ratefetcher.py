19e8dc64a5e40452a1bdeb50a423568ddc906489
Cyril Mouchel <mouchel.cyril@gmail.com>
2014-12-03 12:29:34 +0100

    parsing strings sent as parameters to google
    amend : special characters are replaced by space rather than deleted

--- rate-me-im-famous/ratefetcher.py
+++ rate-me-im-famous/ratefetcher.py
@@ -3,10 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-# used in url encoding
 import urllib
-
-# used to extract number from the content string in the resultStat div from google
 import re
 
 class GoogleRateFetcher(object):
@@ -15,8 +12,8 @@ class GoogleRateFetcher(object):
 		self.__googleURL = 'https://www.google.' + domain + '/#'
 
 	def getPopularity(self, artist, song):
-		query = {'q' : '"' + artist + ' ' + song + '"'}
-
+		# "" are important in the query, it forces google to search the whole string rather than words
+		query = {'q' : '"' + self.__cleanString(artist) + ' ' + self.__cleanString(song) + '"'}
 		# headless selenium thanks to phantomJS integration
 		driver = webdriver.PhantomJS()
 		driver.get(self.__googleURL + urllib.urlencode(query))
@@ -29,3 +26,13 @@ class GoogleRateFetcher(object):
 
 		# findall return a list with one (supposed) element, we want an integer
 		return int(''.join(popularity))
+
+	# google queries are more efficient if we format a bit our artist or song string
+	def __cleanString(self, string = ''):
+		# first, we don't want to see what is written between (), like (feat Sting)...
+		string = re.sub(r'\(.*?\)', '', string)
+		# & is replaced by and
+		string = string.replace('&', 'and')
+		# then, everything that is not digit or alpha or whitespace is replaced with space (google doesn't search for space :))
+		string = re.sub(r'[^a-zA-Z0-9\s]*', ' ', string)
+		return string
